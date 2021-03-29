e31605902c7f91c104841fd5d6337a6157cf9dcb
Vic <victor.lisman@protonmail.com>
2021-03-30 00:23:21 +0300

    Small change

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from pycoingecko import CoinGeckoAPI
 import cbpro
 
-class CryptoCoin(CoinGeckoAPI):
-    def __init__(self, sCoinName, sWalletAdress, bActive, fMaxSell, fMinSell):
-        self.sCoinName = sCoinName
-        self.sWalletAdress = sWalletAdress
-        self.bActive = bActive
-        self.fMaxSell = fMaxSell
-        self.fMinSell = fMinSell
+class CryptoCoin():
+    def __init__(self, sCoinName, bActive, fMaxSell, fMinSell):
+        self.__sCoinName = sCoinName
+        self.__bActive = bActive
+        self.__fMaxSell = fMaxSell
+        self.__fMinSell = fMinSell
 
         print("Coin added")
     
@@ -15,7 +14,7 @@ class CryptoCoin(CoinGeckoAPI):
         print("Coin deleted")
 
     def getCurrentPrice(self):
-        price = CoinGeckoAPI().get_price('ethereum', 'usd')
+        price = CoinGeckoAPI().get_price(self.__sCoinName, 'usd')
         res = []
 
         for key in price.keys():
@@ -26,7 +25,7 @@ class CryptoCoin(CoinGeckoAPI):
     def isWithinParameters(self):
         fCurrent = self.getCurrentPrice()
 
-        if self.fMaxSell < fCurrent or self.fMinSell > fCurrent:
+        if self.__fMaxSell < fCurrent or self.__fMinSell > fCurrent:
             print(fCurrent)
 
     def bSell(self, bActivator):
@@ -34,13 +33,14 @@ class CryptoCoin(CoinGeckoAPI):
             print("sell")
 
 class Profile:
-    def __init__(self, sName, sCardNumber, nCCV, sExpDate, sUserName, sPassword):
-        self.sName = sName
-        self.sCardNumber = sCardNumber
-        self.nCCV = nCCV
-        self.sExpDate = sExpDate
-        self.sUserName = sUserName
-        self.sPassword = sPassword
+    def __init__(self, sName, sWalletAdress, sCardNumber, nCCV, sExpDate, sUserName, sPassword):
+        self.__sName = sName
+        self.__sWalletAdress = sWalletAdress
+        self.__sCardNumber = sCardNumber
+        self.__nCCV = nCCV
+        self.__sExpDate = sExpDate
+        self.__sUserName = sUserName
+        self.__sPassword = sPassword
 
         print("Profile added")
 
@@ -48,10 +48,10 @@ class Profile:
         print("Profile deleted")
 
 def __main__():
-    pyegay = CryptoCoin('ethereum', 'mata', True, 1750, 1700)
-    pyegay.isWithinParameters()
+    a = CryptoCoin('bitcoin', True, 1700, 1700)
+    b = CryptoCoin('ethereum', True, 1, 1)
+    print(a.getCurrentPrice(), b.getCurrentPrice())
     pass
-
 
 if __name__ == '__main__':
     __main__()
